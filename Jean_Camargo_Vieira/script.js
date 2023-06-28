384a0617a8d94a24067047c307a37bafa4578801
caiohamamuraIFSP <hamamura.caio@ifsp.edu.br>
2023-06-27 23:36:59 -0300

    Novo

--- Jean_Camargo_Vieira/script.js
+++ Jean_Camargo_Vieira/script.js
@@ -5,28 +5,37 @@ let casas = document.getElementsByTagName('td');
 let reinicar = document.getElementById('reiniciar')
 reinicar.style.display = 'none'
 let contVelha;
+
+let jogX = document.getElementById('pontoX');
+let jogO = document.getElementById('pontoO');
+let contX = 0;
+let contO = 0;
+jogX.innerHTML = `Jogador X: ${contX}`;
+jogO.innerHTML = `Jogador O: ${contO}`;
 // Grid de casas
 // 0 1 2
 // 3 4 5
 // 6 7 8
 
-mostradorJogador.innerHTML = jogadorAtual;
-
 // Liga todas as células na função jogar
 for (const casa of casas) {
   casa.onclick = jogar;
 }
+
+refresh();
+
 function jogar() {  
   // TODO: verificar se o quadro já não está preenchido!
     if(this.textContent != '')
         return;
   // Coloca o conteúdo na célula atual
+  document.onkeyup = false;
   this.textContent = jogadorAtual;
   trocaTurno();
   trocaJogador();
   mostradorJogador.innerHTML = jogadorAtual;
 }
-//Troca jogador
+
 function trocaJogador() {
   if (jogadorAtual === 'X') {
     jogadorAtual = 'O';
@@ -34,34 +43,41 @@ function trocaJogador() {
     jogadorAtual = 'X';
   }
 }
-//Verifica o fim / troca turno
+
 function trocaTurno() {
 
   let ganhou = verificaFim();
   if (ganhou) {
     cabecalho.innerHTML = `${jogadorAtual} ganhou!`;
     bloqueiaCelulas();
+    if(jogadorAtual === 'X')
+      contX++;
+    else
+      contO++;
+    jogX.innerHTML = `Jogador X: ${contX}`;
+    jogO.innerHTML = `Jogador O: ${contO}`;
   } else
       verificaVelha()
 }
-//Verifica ganhador
+
 function verificaFim() {
-    let posGanha = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+    let posGanha = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]];
     for(i of posGanha) {
         if(casas[i[0]].innerHTML == casas[i[1]].innerHTML && casas[i[1]].innerHTML == casas[i[2]].innerHTML && casas[i[1]].innerHTML !== '')
-            return true;
-    }
-    return false;
+          return true;
+  }
+  return false;
 }
+
 function bloqueiaCelulas() {
   if(verificaFim()) {
       for(i of casas) {
         i.onclick = false
       }
       reinicar.style.display = 'block'
+      document.onkeyup = refresh;
   }
 }
-//verificação velha
 function verificaVelha() {
   contVelha = 0;
   for(i of casas) {
@@ -72,17 +88,15 @@ function verificaVelha() {
   if(contVelha == 9) {
     alert("Deu velha")
     reinicar.style.display = 'block'
-    var img = document.createElement("IMG");
   }
 }
-//reinicia jogo
 function refresh() {
   for(i of casas) {
-    i.textContent = ''
-    i.onclick = jogar
-    jogadorAtual="X"
-    cabecalho.innerHTML = `Jogando:${jogadorAtual}`;
-
+    i.textContent = '';
+    i.onclick = jogar;
+    cabecalho.innerHTML = 'Jogando: <span id="jogador"></span>'
+    mostradorJogador = document.getElementById('jogador');
+    mostradorJogador.innerHTML = jogadorAtual;
 }
-  reinicar.style.display = 'none'
+  reinicar.style.display = 'none';
 }
